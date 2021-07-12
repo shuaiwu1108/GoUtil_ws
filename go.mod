bacca847b1bbde2cb59f7bb018c9ecda9f969720
wslio <wslio@hotmail.com>
2021-07-12 22:45:05 +0800

    Revert "1. 更新依赖"
    This reverts commit df12326db23d6d6b7f3ba95e7dea5f744c51374b.

---
+++ go.mod
@@ -0,0 +1,1 @@
+module "GoUtil_ws"
--- util/DBUtil.go
+++ util/DBUtil.go
@@ -3,7 +3,7 @@ package util
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
-	"model"
+	"github.com/wslio/GoUtil_ws/model"
 	"strconv"
 )
 
