220ef6681de0f61451d65915c7b4989c854f1770
wslio <wslio@hotmail.com>
2021-07-23 17:32:18 +0800

    1. 增加sql执行方法

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # GoUtil_ws
 Go语言，工具包，自用
-# OrcleDBUtil
-## 需要安装oracle客户端，并配置系统环境变量CGO_LDFLAGS
-## windows下需要安装mingw环境
+### 1. OrcleDBUtil
+1.需要安装oracle客户端，并配置系统环境变量CGO_LDFLAGS
+2.windows下需要安装mingw环境
+3.OracleInit
+4.OracelQueryOne
+5.OracelQueryAll
+
+### 2. CommonUtil
+1.提供Pause方法，用于暂停当前窗口而不退出
+
+### 3.DBUtil
+1.MySql的链接操作封装
+2.DbInit
+3.QueryOne
+4.QueryAll
+
+### 4.sql文件夹说明
+1.文件夹内部不要直接放入sql脚本
+2.建立二级目录后，在放入sql脚本
+3.如果有多个脚本，请保证文件排序，以便升序执行
+4.执行方法为DBUtil.ReadSqlExec()
--- util/DBUtil.go
+++ util/DBUtil.go
@@ -2,9 +2,14 @@ package util
 
 import (
 	"GoUtil_ws/model"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"io/ioutil"
+	"path/filepath"
+	"regexp"
 	"strconv"
+	"strings"
 )
 
 var (
@@ -15,6 +20,7 @@ func DbInit(dbInfo model.DBInfo) {
 	database, err := sqlx.Open("mysql", dbInfo.Name+":"+dbInfo.Pass+"@tcp("+dbInfo.Url+":"+strconv.Itoa(dbInfo.Port)+")/"+dbInfo.DbName+"?charset=utf8&parseTime=true")
 	HandleError(err, "open mysql failed, ")
 	db = database
+	fmt.Println("Mysql 初始化完毕！", dbInfo.Url)
 }
 
 func QueryOne(sqlStr string, args ...interface{}) (map[string]string, error) {
@@ -60,4 +66,60 @@ func QueryAll(sqlStr string, args ...interface{}) ([]map[string]string, error) {
 		results = append(results, row)
 	}
 	return results, nil
+}
+
+func ExecSql(sqlStr string, args ...interface{}){
+	rs,err := db.Exec(sqlStr, args...)
+	HandleError(err, "MySql执行出错！")
+	num, _ := rs.RowsAffected()
+	id,_ := rs.LastInsertId()
+	fmt.Println("影响行数：", num, " id：", id)
+}
+
+func PrepareSql(sqlString string){
+	rs,err := db.Prepare(sqlString)
+	HandleError(err, "MySql执行出错！")
+	rs.Exec()
+}
+
+func ReadSqlExec(){
+	dir := GetIniVal("sql", "dir")
+	fmt.Printf("Sql执行目录：%s\n", dir)
+
+	fileInfoList, err := ioutil.ReadDir(dir)
+	HandleError(err, "Sql目录读取失败！")
+
+	reg := regexp.MustCompile(`{.*}`)
+
+	for i := range fileInfoList{
+		fmt.Printf("Sql文件名：%s\n", fileInfoList[i].Name())
+		bytes, err := ioutil.ReadFile(filepath.Join(dir, fileInfoList[i].Name()))
+		HandleError(err, fileInfoList[i].Name()+" 文件读取失败！")
+		allSql := string(bytes)
+		sqlArr := strings.Split(allSql, "@repeat")
+		for i := range sqlArr{
+			sql := sqlArr[i]
+			if len(sql)==0 {
+				continue
+			}
+			result := reg.FindAllString(sql, -1)
+			if len(result)==0{
+				tmpSql := sql[strings.Index(sql,"}")+1:]
+				fmt.Println("执行sql：", tmpSql)
+				ExecSql(tmpSql)
+			}else{
+				tmpRes, err := QueryOne(result[0][1:len(result[0])-1])
+				HandleError(err, "条件Sql执行失败！")
+				coun := tmpRes["count(1)"]
+				fmt.Println("条件sql结果：", coun, "跳过执行")
+				if("1" == coun){
+					continue
+				}else{
+					tmpSql := sql[strings.Index(sql,"}")+1:]
+					fmt.Println("执行sql：", tmpSql)
+					ExecSql(tmpSql)
+				}
+			}
+		}
+	}
 }
